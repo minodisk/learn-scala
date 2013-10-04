ac85e2bcd39be8c9b579ef659a0bbb52123659aa
minodisk <daisuke.mino@gmail.com>
2013-10-04 21:02:13 +0900

    6.5 フィールドの追加

---
+++ src/Bootstrap.scala
@@ -0,0 +1,10 @@
+object Bootstrap {
+  def main(args: Array[String]) {
+    val x = new Rational(1, 3)
+    println(x)
+    val y = new Rational(5, 7)
+    println(y)
+    val z = new Rational(5, 0)
+    println(z)
+  }
+}
---
+++ src/Rational.scala
@@ -0,0 +1,15 @@
+class Rational(n:Int, d:Int) {
+
+  require(d != 0)
+  val numer:Int = n
+  val denom:Int = d
+
+  override def toString = numer + "/" + denom
+
+  def add(that:Rational) =
+    new Rational(
+      numer * that.denom + that.numer * denom,
+      denom * that.denom
+    )
+
+}
