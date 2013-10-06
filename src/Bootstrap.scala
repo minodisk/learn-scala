a9f78e828456341e4b9c1293328bc485809d711c
minodisk <daisuke.mino@gmail.com>
2013-10-06 17:35:49 +0900

    6.6 自己参照

--- src/Bootstrap.scala
+++ src/Bootstrap.scala
@@ -4,7 +4,34 @@ object Bootstrap {
     println(x)
     val y = new Rational(5, 7)
     println(y)
-    val z = new Rational(5, 0)
-    println(z)
+//    val z = new Rational(5, 0)
+//    println(z)
+
+    run6_5
+    run6_6
   }
+
+  def run6_5 = {
+    println("§6.5")
+
+    val oneHalf = new Rational(1, 2)
+    val twoThirds = new Rational(2, 3)
+    val result = oneHalf add twoThirds
+    println(result)
+
+    val r = new Rational(1, 2)
+    println(r.numer)
+    println(r.denom)
+  }
+
+  def run6_6 = {
+    println("§6.6")
+
+    val oneHalf = new Rational(1, 2)
+    val twoThirds = new Rational(2, 3)
+    println(oneHalf lessThan twoThirds)
+    println(twoThirds lessThan oneHalf)
+    println(oneHalf max twoThirds)
+  }
+
 }
--- src/Rational.scala
+++ src/Rational.scala
@@ -6,10 +6,19 @@ class Rational(n:Int, d:Int) {
 
   override def toString = numer + "/" + denom
 
-  def add(that:Rational) =
+  def add(that:Rational) = {
     new Rational(
       numer * that.denom + that.numer * denom,
       denom * that.denom
     )
+  }
+
+  def lessThan(that:Rational) = {
+    this.numer * that.denom < that.numer * this.denom
+  }
+
+  def max(that:Rational)= {
+    if (lessThan(that)) that else this
+  }
 
 }
