070edeeb957eb88d6a7f6da93f3c2fccd9ac1a77
minodisk <daisuke.mino@gmail.com>
2013-10-07 17:48:51 +0900

    8.2 ローカル関数

---
+++ src/Section8_2.scala
@@ -0,0 +1,20 @@
+import scala.io.Source
+
+object Section8_2 {
+
+  def main(args: Array[String]) {
+    progressFile("/Users/minodisk/workspace/learn-scala/resource/lorem_ipsum.txt", 100)
+  }
+
+  def progressFile(filename: String, width: Int) {
+    def processLine(line: String) {
+      if (line.length > width)
+        println(filename + ": " + line.trim)
+    }
+
+    val source = Source.fromFile(filename)
+    for (line <- source.getLines()) processLine(line)
+  }
+
+
+}
