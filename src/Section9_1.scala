18bded0c73edd218b1ce170ea83a0b2a139579ae
minodisk <daisuke.mino@gmail.com>
2013-10-08 16:07:43 +0900

    9.1 重複するコードの削減

--- src/Section9_1.scala
+++ src/Section9_1.scala
@@ -4,8 +4,17 @@ object Section9_1 {
 
   def main(args: Array[String]) {
 
+    println("filesEnding ->")
     for (file <- FileMatcher.filesEnding(".iml"))
-      println(file.getName)
+      println("  " + file.getName)
+
+    println("filesContaining ->")
+    for (file <- FileMatcher.filesContaining("git"))
+      println("  " + file.getName)
+
+    println("filesRegex ->")
+    for (file <- FileMatcher.filesRegex(""".*rc.*"""))
+      println("  " + file.getName)
 
   }
 
@@ -15,20 +24,45 @@ object FileMatcher {
 
   private def filesHere = new File(".").listFiles
 
-  def filesEnding(query:String) =
+  def filesMatching(matcher:(String)=>Boolean) =
     for (
       file <- filesHere
-      if file.getName.endsWith(query)
+      if matcher(file.getName)
     ) yield {
       file
     }
 
+  def filesEnding(query:String) =
+    filesMatching(_.endsWith(query))
+
   def filesContaining(query:String) =
-    for (
-      file <- filesHere
-      if file.getName.contains(query)
-    ) yield {
-      file
-    }
+    filesMatching(_.contains(query))
+
+  def filesRegex(query:String) =
+    filesMatching(_.matches(query))
+
+//  def filesEnding(query:String) =
+//    for (
+//      file <- filesHere
+//      if file.getName.endsWith(query)
+//    ) yield {
+//      file
+//    }
+//
+//  def filesContaining(query:String) =
+//    for (
+//      file <- filesHere
+//      if file.getName.contains(query)
+//    ) yield {
+//      file
+//    }
+//
+//  def filesRegex(query:String) =
+//    for (
+//      file <- filesHere
+//      if file.getName.matches(query)
+//    ) yield {
+//      file
+//    }
 
 }
